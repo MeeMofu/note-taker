c65f59d47187a1d6b04e5b502f7d7ccfe6e00fc7
VTran <MeeMofu@users.noreply.github.com>
2020-10-21 21:37:47 -0400

    Added get api

--- routes/apiRoutes/notesAPI.js
+++ routes/apiRoutes/notesAPI.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const {addNote} = require('../../lib/notesHandling');
+const {addNote,getNote,removeNote} = require('../../lib/notesHandling');
 const {notes} = require('../../db/db.json');
 
 router.post('/notes',(req,res) => {
@@ -10,5 +10,20 @@ router.post('/notes',(req,res) => {
 router.get('/notes',(req,res)=>{
     res.json(notes);
 });
+
+router.get('/notes/:id',(req,res)=>{
+    if (req.query.action === 'remove') {
+        const status = removeNote(req.params.id,notes);
+        if (status === 'failed') res.sendStatus(404);
+        else res.json(notes);
+    }
+    // If there's query action=remove, it will remove the note
+    else {
+        const result = getNote(req.params.id,notes);
+        // If just the id, then it return note with the id
+        if (result) res.json(result);
+        else res.send(404);
+    }
+})
 
 module.exports = router;
